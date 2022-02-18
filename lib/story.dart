0e85b5eeffc3a0e7b22f14b26ba7080ceb0b7c21
dastagg:opus5 <dastagg@stagglinux.com>
2022-02-18 14:44:27 -0500

    Completed: Step 5, 6, and 7.

--- lib/story.dart
+++ lib/story.dart
@@ -1,8 +1,8 @@
 class Story {
   String storyTitle;
-  String choice1Text;
-  String choice2Text;
+  String choice1;
+  String choice2;
 
-  Story(this.storyTitle, this.choice1Text, this.choice2Text);
+  Story({required this.storyTitle, required this.choice1, required this.choice2});
 }
 
